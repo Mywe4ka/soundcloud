9c0d3de268887776ccc9e65b9fcede7cb5535609
Mariya Herasymiv <mherasym@macmini355.local>
2015-05-14 13:39:28 +0300

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sound::Application.config.secret_token = 'eb95df2b837c8b4e6671b42181f04b6f00ef6c26e4dc9f0defe4055f19e71c3afed94f7492791a5a9c53c18a13871b138af4b4420c4b767625b5a2ef7ad6c6af'
